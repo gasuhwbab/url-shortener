b1b8c85cd50fc5bf49e8a19c344541502be2c762
gshwbb <rus.muradov@icloud.com>
2026-02-02 19:33:43 +0300

    database implemented

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gasuhwbab/url-shortener/internal/config"
 	"github.com/gasuhwbab/url-shortener/internal/logger"
+	"github.com/gasuhwbab/url-shortener/internal/storage/sqlite"
 )
 
 func main() {
@@ -10,8 +13,13 @@ func main() {
 	cfg := config.MustLoad()
 	// logger
 	log := logger.SetupLogger(cfg.Env)
-	log.Info("starting app")
 	// storage
+	storage, err := sqlite.New(cfg.Storage_path)
+	if err != nil {
+		log.Error("failed to init storage", logger.ErrorAttr(err))
+		os.Exit(1)
+	}
 
+	_ = storage
 	// server
 }
